bd7c3014f7dccb4005df996997c2108abc486127
dev Phan <lkphandev@gmail.com>
2020-06-22 22:17:15 -0400

    add route

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from datetime import datetime
 # from flask_restful import Resource, Api
 
 app = Flask(__name__)
 # api = Api(app)
+@app.route('/')
+def hello():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 @app.route('/<string:name>')
 def hello(name):
     return "hello %s" %name
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
